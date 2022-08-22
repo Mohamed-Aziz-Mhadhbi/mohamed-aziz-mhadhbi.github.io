7bb2f01227acc352d78179c56747bec57a001eec
mohamed.aziz.mhadhbi <mohamedaziz.mhadhbi@esprit.tn>
2022-08-22 21:31:56 +0100

    home css content

--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 const Social = () => {
   return (
     <div className="home__social">
-        <a href="#" className="home__social-icon" target="_blank">
+        <a href="#bb" className="home__social-icon" target="_blank">
             <i class="uil uil-instagram"></i>
         </a>
 
-        <a href="#" className="home__social-icon" target="_blank">
+        <a href="#bbd" className="home__social-icon" target="_blank">
             <i class="uil uil-dribbble"></i>
         </a>
 
