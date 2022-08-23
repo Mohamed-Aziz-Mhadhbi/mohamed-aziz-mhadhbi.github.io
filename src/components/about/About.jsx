3742ed3c63da765c244c9ae894f5aa6009ef97c3
mohamed.aziz.mhadhbi <mohamedaziz.mhadhbi@esprit.tn>
2022-08-23 22:15:10 +0100

    about.jsx

---
+++ src/components/about/About.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./about.css"
+import AboutImg from "../../assets/about.jpg";
+import CV from "../../assets/Mohamed Aziz Mhadhbi.pdf";
+
+const About = () => {
+  return (
+    <section className="about section" id="about">
+        <h2 className="section__title">About Me</h2>
+        <span className="section__subtitle">My introduction</span>
+
+        <div className="about__container container grid"></div>
+    </section>
+  )
+}
+
+export default About
