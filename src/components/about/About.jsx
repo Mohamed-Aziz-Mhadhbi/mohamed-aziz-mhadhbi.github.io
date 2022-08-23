f251f09ce589cad0249333f812ffcbf2c986bacf
mohamed.aziz.mhadhbi <mohamedaziz.mhadhbi@esprit.tn>
2022-08-23 22:46:39 +0100

    done for today , to do for tomorrow : info

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -12,7 +12,7 @@ const About = () => {
         <div className="about__container container grid">
             <img src={AboutImg} alt="" className="about__img"/>
 
-            <div className="about__data"></div>
+            <div className="about__data"> </div>
         </div>
     </section>
   )
