889dccb774c8b225e884842573cafc63678e5af6
mohamed.aziz.mhadhbi <mohamedaziz.mhadhbi@esprit.tn>
2025-08-15 19:55:11 +0100

    host

--- src/components/qualification/Qualification.jsx
+++ src/components/qualification/Qualification.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Qualification = () => {
   return (
     <section className="contact section" id="contact">    
-    <div>Qualification</div>
+    <div></div>
     </section>
   )
 }
